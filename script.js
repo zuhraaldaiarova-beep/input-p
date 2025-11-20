4a29bfc7d1447dd6cbf5288e270fe974d58e341d
zuhra <zuhraaldaiarova@icloud.com>
2025-11-20 21:10:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// document.body.addEventListener("" function{
+//     alert("добро пожаловать пользователь")
+// })
+
+
+let inp=document.getElementById("html_in")
+let p=document.getElementById("result")
+
+inp.addEventListener("input",()=>{
+ result .innerHTML=(html_in.value)
+
+})
+
+
+// let btn=document.getElementById("btn")
+// let p=document.getElementById("psh")
+
+// btn.addEventListener('click',()=> {
+//     p.innerHTML="Hello karakol"
+// })
+
+
